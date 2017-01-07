dce8613a0f65f5ba9023875c992325fb88f45483
Gudbrandr <github.jpq@gishpuppy.com>
2017-01-07 14:44:17 +0000

    Changed the directory structure to match OpenBVE as I don't see much value in being able to build it separately, given the dependencies.

---
+++ source/RouteChecker/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle ("RouteChecker")]
+[assembly: AssemblyProduct ("RouteChecker")]
+[assembly: AssemblyCopyright ("(Public Domain) https://github.com/Gudbrandr/RouteChecker")]
+[assembly: ComVisible (false)]
+[assembly: Guid ("a7b4613a-1f4d-4a05-919d-0fc469ecc452")]
+[assembly: AssemblyVersion ("1.4.5.0")]
+[assembly: AssemblyFileVersion ("1.4.5.0")]
